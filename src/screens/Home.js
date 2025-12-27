27bf6356428b44d9423b99c6032d96152496c2c2
Dakshina-Migara <dakshinamigara666@gmail.com>
2025-12-27 11:20:38 +0530

    download the dependencies

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,10 +1,20 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 
 export default function Home() {
-  return (
-    <View>
-      <Text>Home</Text>
-    </View>
-  )
+    return (
+        <View>
+            <Text style={styles.title}>Home</Text>
+        </View>
+    );
 }
+
+const styles = StyleSheet.create({
+    title: {
+        color: 'red',
+        textAlign: 'center',
+        marginTop: 50,
+        fontSize: 30,
+        fontWeight: 'bold',
+    },
+});
